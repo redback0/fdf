036515991022ec2439722a529bb14658efc2805f
Nathaniel Jackson <njackson@student.42Adel.org.au>
2024-05-14 16:55:32 +0930

    Read map from file

--- fdf.c
+++ fdf.c
@@ -6,16 +6,18 @@
 /*   By: njackson <njackson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 23:38:37 by njackson          #+#    #+#             */
-/*   Updated: 2024/05/14 15:42:17 by njackson         ###   ########.fr       */
+/*   Updated: 2024/05/14 16:45:22 by njackson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
 	t_fdf_dat	dat;
 
+	if (argc != 2)
+		return (0); // PRINT ERROR
 	dat.mlx = mlx_init();
 	dat.height = 600;
 	dat.width = 800;
@@ -23,14 +25,69 @@ int	main(void)
 	dat.img_dat = (unsigned int *)mlx_get_data_addr(dat.img,
 			&dat.bpp, &dat.sl, &dat.endian);
 	dat.img_size = (dat.sl / (dat.bpp / 8)) * dat.height;
-	ft_log(1, "bpp: %d\nsize_line: %d\nendian: %d\nimg_size: %d\n",
+	ft_log(1, "IMAGE INFO\nbpp: %d\nsize_line: %d\nendian: %d\nimg_size: %d\n",
 		dat.bpp, dat.sl, dat.endian, dat.img_size);
 	dat.win = mlx_new_window(dat.mlx, dat.width, dat.height, "");
+	get_map(argv[1], &dat);
 	redraw(&dat);
 	mlx_put_image_to_window(dat.mlx, dat.win, dat.img, 0, 0);
 	mlx_key_hook(dat.win, &on_key_pressed, &dat);
 	ft_log(1, "STARTING LOOP\n");
 	mlx_loop(dat.mlx);
+}
+
+int	get_map(char *file, t_fdf_dat *dat)
+{
+	char	**split;
+	char	*line;
+	int		fd;
+	int		i;
+	int		j;
+
+	i = 0;
+	fd = open(file, O_RDONLY);
+	line = get_next_line(fd);
+	while (line)
+	{
+		free(line);
+		line = get_next_line(fd);
+		i++;
+	}
+	close(fd);
+	dat->map = (int **)malloc(i * sizeof(int *));
+	dat->map_x = i;
+	dat->map_y = 0;
+	ft_log(1, "STARTING MAP LOOP\n");
+	fd = open(file, O_RDONLY);
+	line = get_next_line(fd);
+	while (line)
+	{
+		split = ft_split(line, ' ');
+		free(line);
+		j = -1;
+		while (split[++j])
+			;
+		if (!dat->map_y)
+			dat->map_y = j;
+		else if (dat->map_y != j)
+		{
+			ft_log(3, "BAD AMOUNT OF WORDS ON LINE\n");
+			return (1); // CLEAR MEMORY
+		}
+		dat->map[i] = (int *)malloc(dat->map_y * sizeof(int));
+		j = 0;
+		while (split[j])
+		{
+			dat->map[i][j] = ft_atoi(split[j]);
+			j++;
+		}
+		ft_split_free(split, free);
+		ft_log(1, "GOT ROW\n");
+		line = get_next_line(fd);
+	}
+	ft_log(1, "GOT ALL ROWS\n");
+	close(fd);
+	return (0);
 }
 
 int	on_key_pressed(int keycode, t_fdf_dat *dat)
@@ -46,15 +103,26 @@ int	on_key_pressed(int keycode, t_fdf_dat *dat)
 
 int	redraw(t_fdf_dat *dat)
 {
-	double	size = 50;
-	double	cube[][3] = {{0, 0, 0}, {0, 0, size}, {0, size, 0}, {0, size, size},
-			{size, 0, 0}, {size, 0, size}, {size, size, 0}, {size, size, size}};
-	int		x;
-	int		y;
-	int		i;
+	//int		x;
+	//int		y;
+	//int		xi;
+	//int		zi;
 
 	ft_log(1, "REDRAW\n");
 	// CHANGE IMAGE HERE
+	mlx_put_image_to_window(dat->mlx, dat->win, dat->img, 0, 0);
+	return (0);
+}
+// x' = (x - z) / sqrt(2)
+// y' = (x + 2y + z) / sqrt(6)
+
+//dat->img_dat[y * dat->width + x]
+
+// CUBE POINTS
+/*
+	double	size = 50;
+	double	cube[][3] = {{0, 0, 0}, {0, 0, size}, {0, size, 0}, {0, size, size},
+			{size, 0, 0}, {size, 0, size}, {size, size, 0}, {size, size, size}};
 	i = 0;
 	while (i < 8)
 	{
@@ -65,12 +133,9 @@ int	redraw(t_fdf_dat *dat)
 		ft_log(1, "x: %d\ny: %d\n", x, y);
 		i++;
 	}
-	mlx_put_image_to_window(dat->mlx, dat->win, dat->img, 0, 0);
-	return (0);
-}
+*/
+
 // ISOMETRIC CALCULATION
-// x' = (x - z) / sqrt(2)
-// y' = (x + 2y + z) / sqrt(6)
 // A cross from corner to corner, colors red at the top, to cyan at the bottom
 /*
 	d = (double)dat->height / (double)dat->width;
--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: njackson <njackson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 00:11:50 by njackson          #+#    #+#             */
-/*   Updated: 2024/05/14 15:36:24 by njackson         ###   ########.fr       */
+/*   Updated: 2024/05/14 16:30:43 by njackson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include "libft.h"
 # include "mlx.h"
 # include <math.h>
+# include <fcntl.h>
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
@@ -43,8 +44,12 @@ typedef struct s_fdf_dat
 	int				bpp;
 	int				sl;
 	int				endian;
+	int				**map;
+	int				map_x;
+	int				map_y;
 }	t_fdf_dat;
 
+int		get_map(char *file, t_fdf_dat *dat);
 int		on_key_pressed(int keycode, t_fdf_dat *dat);
 int		redraw(t_fdf_dat *dat);
 
